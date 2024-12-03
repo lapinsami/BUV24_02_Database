e117236cd70d5f5b162b3d3f503bd52296afaff8
Sami Lappalainen <lappalainen.sami@outlook.com>
2024-12-03 23:28:00 +0100

    set id to -1 in constructor

--- DatabaseAssignment/Customer.cs
+++ DatabaseAssignment/Customer.cs
@@ -1,8 +1,8 @@
 namespace DatabaseAssignment;
 
-public class Customer(int id, string firstName, string lastName, string email)
+public class Customer(string firstName, string lastName, string email)
 {
-    public int Id { get; set; } = id;
+    public int Id { get; set; } = -1;
     public string FirstName { get; set; } = firstName;
     public string LastName { get; set; } = lastName;
     public string Email { get; set; } = email;
--- DatabaseAssignment/Product.cs
+++ DatabaseAssignment/Product.cs
@@ -1,8 +1,8 @@
 namespace DatabaseAssignment;
 
-public class Product(int id, string name, string category, decimal price)
+public class Product(string name, string category, decimal price)
 {
-    public int Id { get; set; } = id;
+    public int Id { get; set; } = -1;
     public string Name { get; set; } = name;
     public string Category { get; set; } = category;
     public decimal Price { get; set; } = price;
